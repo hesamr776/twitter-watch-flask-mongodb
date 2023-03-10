46fd443468e9bfd6b718be5f8999536e99ba16e9
hesamr776 <h.rajaei@wallex.net>
2023-03-10 11:41:33 +0000

    feat: views: account

--- counter/models.py
+++ counter/models.py
@@ -1,5 +1,21 @@
 from application import db
 
-
-class Counter(db.Document):  # type: ignore
+class Counter(db.Document):
     count = db.IntField(db_field="c")
+
+class Accounts(db.Document):
+    name = db.StringField(required=True)
+    username = db.StringField(required=True)
+    sentiment = db.BooleanField()
+
+# class Replies(db.EmbeddedDocument):
+#     author = db.StringField(required=True)
+#     text = db.StringField(max_length=120, required=True)
+#     sentiment = db.BooleanField()
+
+# class Tweets(db.Document):
+#     owner = db.ReferenceField(Accounts)
+#     title = db.StringField(max_length=120, required=True)
+#     sentiment = db.BooleanField()
+#     replies = db.ListField(db.EmbeddedDocumentField(Replies))
+      
--- counter/views.py
+++ counter/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-from counter.models import Counter
+from counter.models import Counter, Accounts 
 
 counter_app = Blueprint("counter_app", __name__)
 
@@ -16,3 +16,60 @@ def init():
         counter.count = 1
         counter.save()
     return "<h1>Counter: " + str(counter.count) + "</h1>"
+
+@counter_app.route("/accounts")
+def accounts():
+    accounts = Accounts.objects.all()
+    if  len(accounts) > 0:
+        return accounts
+    
+    Accounts(name='Elon Musk', username='elonmusk').save()
+    Accounts(name='Barack Obama', username='barackobama').save()
+    Accounts(name='Yann Lecun', username='yannlecun').save()
+    return Accounts.objects.all()
+
+
+
+@counter_app.route('/audience/<username>')
+def audience(username):
+    return [
+        { "id": "elonmusk", "avatar": "/elonmusk.jpg" },
+        { "id": "barackobama", "avatar": "/barackobama.jpg" },
+        { "id": "yannlecun", "avatar": "/yannlecun.jpg" }
+    ]
+
+
+@counter_app.route('/sentiment')
+def sentiment():
+    return 'sentiment'
+
+
+@counter_app.route('/tweets/<username>')
+def tweets(username):
+    return [
+        {
+            "username": username,
+            "id": "1234567",
+            "avatar": "/elonmusk.jpg",
+            "date": "10h",
+            "text": 'just human rights',
+            "sentiment": 1
+        },
+        {
+            "username": username,
+            "id": "8765432",
+            "avatar": "/elonmusk.jpg",
+            "date": "1d",
+            "text": 'you are bad',
+            "sentiment": 0
+        },
+        {
+            "username": username,
+            "id": "12345678",
+            "avatar": "/elonmusk.jpg",
+            "date": "2d",
+            "text": 'normal tweet message',
+            "sentiment": 1
+        }
+    ]
+   
