5f48dd6d489220ed02853c46ecc324937f582e0d
hesamr776 <h.rajaei@wallex.net>
2023-03-11 13:10:42 +0000

    feat: db queries

--- counter/views.py
+++ counter/views.py
@@ -1,7 +1,8 @@
+import json
 from flask import Blueprint, jsonify
 from flask_cors import CORS
 
-from counter.models import Counter, Accounts 
+from counter.models import Counter, Accounts, Audience, Tweets, Reply
 
 counter_app = Blueprint("counter_app", __name__)
 cors = CORS(counter_app)
@@ -21,56 +22,29 @@ def init():
 @counter_app.route("/accounts")
 def accounts():
     accounts = Accounts.objects.all()
-    if  len(accounts) > 0:
-        return jsonify(accounts)
+
+    return jsonParse(accounts)    
     
-    Accounts(name='Elon Musk', username='elonmusk').save()
-    Accounts(name='Barack Obama', username='barackobama').save()
-    Accounts(name='Yann Lecun', username='yannlecun').save()
-    return jsonify(Accounts.objects.all())
-
-
+@counter_app.route("/update")
+def update():
+    Accounts.objects(username="yannlecun").update(set__username='ylecun')
+    return jsonParse(Accounts.objects.all())
 
 @counter_app.route('/audience/<username>')
 def audience(username):
-    return jsonify([
-        { "username": "elonmusk", "avatar": "/elonmusk.jpg" },
-        { "username": "barackobama", "avatar": "/barackobama.jpg" },
-        { "username": "yannlecun", "avatar": "/yannlecun.jpg" }
-    ])
-
+    account = Accounts.objects(username=username).first()
+    
+    return jsonParse(account.audience)
 
 @counter_app.route('/sentiment')
 def sentiment():
     return 'sentiment'
 
-
 @counter_app.route('/tweets/<username>')
 def tweets(username):
-    return jsonify([
-        {
-            "username": username,
-            "id": "1234567",
-            "avatar": "/elonmusk.jpg",
-            "date": "10h",
-            "text": 'just human rights',
-            "sentiment": 1
-        },
-        {
-            "username": username,
-            "id": "8765432",
-            "avatar": "/elonmusk.jpg",
-            "date": "1d",
-            "text": 'you are bad',
-            "sentiment": 0
-        },
-        {
-            "username": username,
-            "id": "12345678",
-            "avatar": "/elonmusk.jpg",
-            "date": "2d",
-            "text": 'normal tweet message',
-            "sentiment": 1
-        }
-    ])
-   
+    account = Accounts.objects(username=username).first()
+
+    return jsonParse(account.tweets)
+  
+def jsonParse(query_set):
+    return jsonify(query_set)
