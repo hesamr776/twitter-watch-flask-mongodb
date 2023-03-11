577009ca361be506f8c76d55db52c22e356d31e8
hesamr776 <h.rajaei@wallex.net>
2023-03-11 13:11:20 +0000

    feat: api: data: to store data from file to mongo db

--- counter/views.py
+++ counter/views.py
@@ -7,6 +7,55 @@ from counter.models import Counter, Accounts, Audience, Tweets, Reply
 counter_app = Blueprint("counter_app", __name__)
 cors = CORS(counter_app)
 
+@counter_app.route("/data/<username>")
+def data(username):
+    original_tweets = open(f'./data/{username}.json')
+    original_data = json.load(original_tweets)
+
+    reply_tweets = open(f'./data/replyTo-{username}.json')
+    reply_data = json.load(reply_tweets)
+
+    map_to_original = {}
+    usernames = []
+    for reply in reply_data['replies']:
+        map_to_original[reply['tweetId']] = reply['replies']
+
+        for aud in reply['replies']:
+            usernames.append(aud['username'])
+
+    usernamesSet = set(usernames)
+    audienceReplyCount = {}
+
+    for uname in usernamesSet:
+        repliesCount = usernames.count(uname)
+        audienceReplyCount[uname] = repliesCount
+
+    audiences = sorted(audienceReplyCount.items(), key=lambda item: item[1], reverse=True)
+    filteredAudiences = audiences[:20]
+    accountAudiences = []
+    for aud in filteredAudiences:
+        accountAudiences.append(Audience(username=aud[0], avatar='', replyCount=aud[1]))
+
+    replies = []
+    tweets = []
+    for tweet in original_data['tweets']:
+        tweet_replies = [] 
+        for reply in map_to_original[tweet['id']]:
+            tweet_replies.append(Reply(username=reply['username'], avatar='/barackobama.jpg', text=reply['text'], date=reply['date'], sentiment=reply['sentiment']))
+
+        tweets.append(Tweets(text=tweet['text'], date=tweet['date'], sentiment=tweet['sentiment'], replies=tweet_replies))
+
+    account = Accounts.objects(username=username).first()
+    account.tweets = tweets
+    account.audience = accountAudiences
+    account.save()
+    
+    original_tweets.close()
+    reply_tweets.close()
+
+    return jsonParse(account)
+
+
 @counter_app.route("/")
 def init():
     counter = Counter.objects.all().first()
